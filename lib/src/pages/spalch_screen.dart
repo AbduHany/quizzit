816940c7218d231a75015bce1bfbe5742bf39797
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-06-11 10:53:00 +0300

    splash screen setup

---
+++ lib/src/pages/spalch_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:quizzit/src/pages/start_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: const Center(
+      child: Text("Splash Screen"),
+    ));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Future.delayed(
+        const Duration(seconds: 5),
+        () => {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const StartPage()),
+                  (Route<dynamic> route) => false)
+            });
+  }
+}
---
+++ lib/src/pages/start_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text("Start Page"),
+    ));
+  }
+}
